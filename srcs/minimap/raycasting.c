6b1cac67af89c8b923664e1c2e5e91e93f88c6e7
Yee Jin <yeejin0406@gmail.com>
2022-11-03 13:38:33 +0800

    done rotation and player movement

--- srcs/minimap/raycasting.c
+++ srcs/minimap/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: yang <yang@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 19:44:33 by yang              #+#    #+#             */
-/*   Updated: 2022/10/30 22:43:29 by yang             ###   ########.fr       */
+/*   Updated: 2022/11/02 21:42:24 by yang             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 #include "minimap.h"
 
 #define FOV 66.0
-#define STRIPE 800.0
+#define STRIPE 400.0
 #define h 500
 #define MAP_WIDTH 20.0
 #define MAP_HEIGHT 15.0
@@ -32,7 +32,7 @@ char *map_1[15] = {
 	"101000000000010", // 5
 	"101000000000001", // 6
 	"100001000000001", // 7
-	"111111000W00001", // 8
+	"111111000W00001", // 8 (8,9)
 	"100100000001111", // 9
 	"100010000000001", // 10
 	"100100000000001", // 11
@@ -58,22 +58,25 @@ void dda_ver(t_matrix matrix, t_minimap *minimap, int color)
 void draw_3D(t_game *game, t_minimap *minimap)
 {
 	int x = -1;
-
 	while (++x < STRIPE)
 	{
 		double angle;
 		angle = (double)(minimap->player_angle + (FOV / 2)) - ((double)x * (FOV / STRIPE));
+		if (angle < 0.0)
+			angle += 360.0;
+		else if (angle > 360.0)
+			angle -= 360.0;
 		printf("angle: %f\n", angle);
 		double rayDirX = cos(deg_to_rad(angle));
 		double rayDirY = sin(deg_to_rad(angle));
 		printf("\nrayDirX: %f\t rayDirY: %f\n", rayDirX, rayDirY);
-		t_matrix draw_ray;
-		draw_ray.x0 = minimap->player_pos.x0;
-		draw_ray.y0 = minimap->player_pos.y0;
-		draw_ray.x1 = draw_ray.x0 + ((rayDirX)*5);
-		draw_ray.y1 = draw_ray.y0 - ((rayDirY)*5);
+		// t_matrix draw_ray;
+		// draw_ray.x0 = minimap->player_pos.x0;
+		// draw_ray.y0 = minimap->player_pos.y0;
+		// draw_ray.x1 = draw_ray.x0 + ((rayDirX)*5);
+		// draw_ray.y1 = draw_ray.y0 - ((rayDirY)*5);
 		// dda(draw_ray, minimap, 0xFF0000);
-		int mapX = (int)minimap->player_pos.x0;
+		int mapX = (int)minimap->player_pos.x0; // need to know the exact player pos (0.x? and facing direction to get exact starting point)
 		int mapY = (int)minimap->player_pos.y0;
 		// printf("mapX: %d\t mapY: %d\n", mapX, mapY);
 		// printf("mapX: %d\t mapY: %d\n", mapX, mapY);
@@ -83,17 +86,21 @@ void draw_3D(t_game *game, t_minimap *minimap)
 		// double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
 		double deltaDistX = (rayDirX == 0) ? 1e30 : sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
 		double deltaDistY = (rayDirY == 0) ? 1e30 : sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
+		printf("player X: %f\t Y: %f\n", minimap->player_pos.x0, minimap->player_pos.y0);
 		printf("deltaDistX: %f\t deltaDistY: %f\n", deltaDistX, deltaDistY);
 		double perpWallDist;
 		int stepX;
 		int stepY;
 
-		int hit = 0; // was there a wall hit?
-		int side;	 // was a NS o
+		int hit = 0;
+		int side;
+		int ray_deduct_x = 0;
+		int ray_deduct_y = 0;
 		if (rayDirX < 0)
 		{
 			stepX = -1;
 			sideDistX = (minimap->player_pos.x0 - mapX) * deltaDistX;
+			ray_deduct_x = 1;
 		}
 		else
 		{
@@ -109,6 +116,7 @@ void draw_3D(t_game *game, t_minimap *minimap)
 		{
 			stepY = -1;
 			sideDistY = (minimap->player_pos.y0 - mapY) * deltaDistY;
+			ray_deduct_y = 1;
 		}
 		// if (sideDistX == 0)
 		// 	sideDistX += deltaDistX;
@@ -153,13 +161,13 @@ void draw_3D(t_game *game, t_minimap *minimap)
 				// 	dist = fabs(mapY - draw_ray.y0 + 1) * deltaDistY;
 				if (side == 0)
 				{
-					draw_ray.x1 = draw_ray.x0 + (rayDirX * fabs(mapX - draw_ray.x0 + 1) * deltaDistX);
-					draw_ray.y1 = draw_ray.y0 - (rayDirY * fabs(mapX - draw_ray.x0 + 1) * deltaDistX);
+					draw_ray.x1 = draw_ray.x0 + (rayDirX * fabs(mapX - draw_ray.x0 + ray_deduct_x) * deltaDistX);
+					draw_ray.y1 = draw_ray.y0 - (rayDirY * fabs(mapX - draw_ray.x0 + ray_deduct_x) * deltaDistX);
 				}
 				else
 				{
-					draw_ray.x1 = draw_ray.x0 + (rayDirX * fabs(mapY - draw_ray.y0) * deltaDistY);
-					draw_ray.y1 = draw_ray.y0 - (rayDirY * fabs(mapY - draw_ray.y0) * deltaDistY);
+					draw_ray.x1 = draw_ray.x0 + (rayDirX * fabs(mapY - draw_ray.y0 + ray_deduct_y) * deltaDistY);
+					draw_ray.y1 = draw_ray.y0 - (rayDirY * fabs(mapY - draw_ray.y0 + ray_deduct_y) * deltaDistY);
 				}
 				dda(draw_ray, minimap, 0x00FF00);
 				printf("x: %d\t hit!!\n", x);
